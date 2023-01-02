cf051dd3976f3489400e426b2fb1b0963b6e6551
samuriverosb <samuel.riverosb@gmail.com>
2023-01-01 21:09:48 -0300

    build commit

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ import morgan from 'morgan';
 import { Server as SocketServer } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 5345;
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const server = http.createServer(app)
 const io = new SocketServer(server, {
   cors: {
@@ -25,7 +28,7 @@ io.on('connection', (socket) => {
   })
 })
 
-
+app.use(express.static(join(__dirname, '../client/dist')))
 
 server.listen(PORT);
 console.log("Server listening on port", PORT);
